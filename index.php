f8e41b3f9939c36dcac7ab65b176155bd2e5ce2b
Mustafa Abu Hamad <mustafa.ah@baxter-academy.org>
2018-11-09 12:50:17 -0500

    more front-side updates
    not much but decent

--- index.php
+++ index.php
@@ -53,20 +53,26 @@
 						<b class="TextStyle_Title">Recommended Artists</b>
 					</div>
 					<div class="Recommended_Artists_Container">
-						<div class="Recommended_Artist Young Thug">
+						<div class="Recommended_Artist">
 							<div style="background-image: url(client/images/ytj.jpg);" class="Artist_IMG"></div>
 						</div>
-						<div class="Recommended_Artist Tyler The Creator">
+						<div class="Recommended_Artist">
 							<div style="background-image: url(client/images/ttcfb.png);" class="Artist_IMG"></div>
 						</div>
-						<div class="Recommended_Artist Ariana Grande">
+						<div class="Recommended_Artist">
 							<div style="background-image: url(client/images/ag.jpg);" class="Artist_IMG"></div>
 						</div>
-						<div class="Recommended_Artist Chief Keef">
+						<div class="Recommended_Artist">
 							<div style="background-image: url(client/images/ck.jpg);" class="Artist_IMG"></div>
 						</div>
-						<div class="Recommended_Artist Drake">
+						<div class="Recommended_Artist">
 							<div style="background-image: url(client/images/drakepfp.jpg);" class="Artist_IMG"></div>
+						</div>
+						<div class="Recommended_Artist">
+							<div style="background-image: url(client/images/travis.jpg);" class="Artist_IMG"></div>
+						</div>
+						<div class="Recommended_Artist">
+							<div style="background-image: url(client/images/ba.jpg);" class="Artist_IMG"></div>
 						</div>
 					</div>
 				</div>
---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+	// require("server/database.php");
+
+	require('_includes/header.php');
+?>
+		<div class="Client_Page_Container">
+
+		</div>
+		<?php require('_includes/footer.php');?>
+	</body>
+</html>
