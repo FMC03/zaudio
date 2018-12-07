95ebd508fb8a837e521990a0109c218434d6dffe
Fred Czerwinski <frederick.czerwinski@baxter-academy.org>
2018-12-07 08:57:10 -0500

    new icons

--- _includes/header.php
+++ _includes/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<html lang="eng">
 		<meta charset="UTF-8">
-		<title>MSA - Music Streaming App</title>
+		<title>Nigger - Music Streaming App</title>
 		<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500,100" rel="stylesheet">
 		<link href="client/css/html.css?v=<?php echo time();?>" type="text/css" rel="stylesheet">
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
