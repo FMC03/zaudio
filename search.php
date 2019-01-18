b96210f62db1ed5ca2770f379cfd7265f4125210
Fred <boromirfmcjr91@gmail.com>
2019-01-18 09:47:26 -0500

    I put a spot to program search horizontal scrolling

--- search.php
+++ search.php
@@ -11,137 +11,45 @@
 				<div class="Search_Results_Container">
 					<div class="Results_Container_Group">
 						<div class="Search_Group">Songs</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Sicko Mode</div>
-							<div class="Result_SubHeading">Travis Scott</div>
+						<div class="Results_Group">
+							<div class="Results">
+								<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
+								<div class="Result_Heading">Sicko Mode</div>
+								<div class="Result_SubHeading">Travis Scott</div>
+							</div>
 						</div>
 					</div>
 					<div class="Results_Spacer"></div>
 					<div class="Results_Container_Group">
 						<div class="Search_Group">Albums</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Astroworld</div>
-							<div class="Result_SubHeading">Travis Scott</div>
+						<div class="Results_Group">
+							<div class="Results">
+								<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
+								<div class="Result_Heading">Astroworld</div>
+								<div class="Result_SubHeading">Travis Scott</div>
+							</div>
 						</div>
 					</div>
 					<div class="Results_Spacer"></div>
 					<div class="Results_Container_Group">
 						<div class="Search_Group">Artists</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Travis Scott</div>
-							<div class="Result_SubHeading">Rap</div>
+						<div class="Results_Group">
+							<div class="Results">
+								<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
+								<div class="Result_Heading">Travis Scott</div>
+								<div class="Result_SubHeading">Rap</div>
+							</div>
 						</div>
 					</div>
 					<div class="Results_Spacer"></div>
 					<div class="Results_Container_Group">
 						<div class="Search_Group">Playlists</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
-						</div>
-						<div class="Results">
-							<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
-							<div class="Result_Heading">Playlist Name</div>
-							<div class="Result_SubHeading">User123</div>
+						<div class="Results_Group">
+							<div class="Results">
+								<div style="background-image: url(client/images/sm.jpg);" class="Song_Picture"></div>
+								<div class="Result_Heading">Playlist Name</div>
+								<div class="Result_SubHeading">User123</div>
+							</div>
 						</div>
 					</div>
 					<div class="Bottom_Box"></div>
