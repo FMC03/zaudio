3212da12d1267da9a0f79250e85e3c676ce453f2
Rob Korobkin <rob.korobkin@gmail.com>
2019-03-22 09:19:54 -0400

    sprites tutorial

--- tutorial/sprites.php
+++ tutorial/sprites.php
@@ -4,8 +4,23 @@
 	<title>LMAO</title>
 </head>
 <body>
-	<img src="icons.png" />
 
-	<div style="background: url(icons.png); height: 180px; width: 200px; background-position: 100px 0; border: solid 1px #ccc;"  />
+	<div style="position: relative; width: 619px; height: 330px">
+
+		<img src="icons.png" style="position: absolute; z-index: 1" />
+		
+		<div style="height: 170px; width: 180px; border: solid 1px red; position: absolute; z-index: 2;
+
+			left: 180px; top: 170px;
+
+		"></div>
+	</div>
+
+	<div style="margin: 100px; background: url(icons.png) no-repeat; height: 170px; width: 180px; border: solid 1px #ccc;
+
+		background-size: 310px 175px;
+
+/*		background-position: -180px -170px; 
+*/	"  />
 </body>
 </html>
