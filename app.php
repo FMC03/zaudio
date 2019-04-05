56e93d6fc1f797019deda182739d3a972113d5ac
Fred <boromirfmcjr91@gmail.com>
2019-04-05 09:28:12 -0400

    App Stuff n' Thangs

---
+++ app.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Basic JS Architecture</title>
+
+			<link href="./client/css/html.css" type="text/css" rel="stylesheet">
+			<link href="./music.css" type="text/css" rel="stylesheet">
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<script>
+
+
+		var numbers = {
+			key1 : 'value 1',
+			key2 : 'value 2'
+		}
+
+
+		// DEFINE OUR DATA OBJECT
+		// JAVASCRIPT OBJECT NOTATION (JSON)
+
+		var songs = 	[
+														
+							// songs[0]
+							{
+								song: "Gucci Gang",
+								artist : "Lil Pump",
+								img: 'client/images/gc.jpg',
+								likes: "5",
+								replays: "5k+"
+							},
+
+							
+							// songs[1]
+							{
+								song: "Feels Like Summer",
+								artist: "Childish Gambino",
+								img: 'client/images/fls.png',
+								likes: "1.3k+",
+								replays: "5k+"
+							},
+
+							// songs[2]
+							{
+								song: "XO Tour Lif3",
+								artist: "Lil Uzi Vert",
+								img : 'client/images/xotl.jpg',
+								likes: "2k+",
+								replays: "5k+",
+							}
+						];
+
+
+
+		// start our empty html string
+		/*<div class="Recommended_Song">
+							<div style="background-image: url(client/images/gc.jpg);"  class="Song_IMG"></div>
+							<div class="Song_Information">								
+								<b class="Song_Name">Gucci Gang</b>
+								<b class="Song_Artist">Lil Pump</b>
+								<div class="Song_Data">
+									<div class="Liked">
+										<img src="client/images/icons/liked.png" />
+										<b class="Song_Liked_Amount">5</b>
+									</div>
+									<div class="Replay">
+										<img src="client/images/icons/replays.png" />
+										<b class="Song_Replay_Amount">5k+</b>
+									</div>
+								</div>
+							</div>
+						</div> */
+
+		var html = '';
+
+
+
+		for(var i = 0; i < songs.length; i++){
+
+			var song = songs[i];
+			
+			html 	+= 	'<div class="Recommended_Song">' + 	
+							'<div style="background-image: url('+ song.img +');"  class="Song_IMG"></div>' +
+							'<div class="Song_Information">'+
+								'<b class="Song_Name">' + song.song	 + '</b>' +
+								'<b class="Song_Artist">' + song.artist	 + '</b>' +
+								'<div class="Song_Data">' + 
+									'<div class="Liked">' +
+										'<img src="client/images/icons/liked.png" />' +
+										'<b class="Song_Replay_Amount">' + song.likes + '</b>' +
+									'</div>' +
+									'<div class="Replay">' +
+										'<img src="client/images/icons/replays.png" />' +
+										'<b class="Song_Replay_Amount">' + song.replays + '</b>' +
+									'</div>'+
+								'</div>' +
+						'</div>';
+
+		}
+
+
+		
+
+		
+
+	
+		// WAIT UNTIL THE PAGE HAS LOADED
+		$(function(){
+					
+			// AND THEN RUN THIS...
+			var element = $('#html_frame');
+
+			element.html(html);
+
+			element.css('color', 'red');
+
+			element.click(function(){
+				alert('click!!!')
+			})
+
+		});
+
+
+	</script>
+
+</head>
+<body>
+	<div class="Client_Page_Container">
+		<div class="Client_Home_Container">
+			<div id="html_frame"></div>
+		</div>
+	</div>
+	
+</body>
+</html>
--- tutorial/sprites.php
+++ tutorial/sprites.php
@@ -11,18 +11,55 @@
 		
 		<div style="height: 170px; width: 180px; border: solid 1px red; position: absolute; z-index: 2;
 
-			left: 180px; top: 170px;
+			left: 170px; top: 170px;
 
 		"></div>
 	</div>
 
 	<div style="margin: 100px; background: url(icons.png) no-repeat; border: solid 1px #ccc;
 
-		background-size: 310px 175px;
+		
 
 		height: 170px; width: 180px; 
 
 /*		background-position: -180px -170px; 
+*/	"  />
+
+	<div style="height: 100px;"></div>
+
+	<div style="margin: 100px; background: url(icons.png) no-repeat; height: 160px; width: 160px; border: solid 1px #ccc; position: relative; 
+
+		
+
+	background-position: -10px -175px; 
+	/*
+*/	"  />
+	<div style="height: 100px;"></div>
+
+	<div style="margin: 100px; background: url(icons.png) no-repeat; height: 170px; width: 190px; border: solid 1px #ccc; position: relative; 
+
+		
+
+	background-position: 3px top; 
+	/*
+*/	"  />
+
+	<div style="height: 100px;"></div>
+
+	<div style="margin: 100px; background: url(icons.png) no-repeat; height: 170px; width: 190px; border: solid 1px #ccc; position: relative; 
+
+		
+	background-position: -206px top;
+	/*
+*/	"  />
+
+	<div style="height: 100px;"></div>
+
+	<div style="margin: 100px; background: url(icons.png) no-repeat; height: 170px; width: 190px; border: solid 1px #ccc; position: relative; 
+
+		
+	background-position: -400px top;
+	/*
 */	"  />
 </body>
 </html>
