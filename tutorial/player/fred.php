6944e0ad989715d01d7ec4c94fd8b4732ba87ae6
fred <boromirfmcjr91@gmail.com>
2019-05-09 19:13:06 -0400

    Seek bar, mute toggle, play pause, volume slider
    seek bar html still has an error but the rest is functional

---
+++ tutorial/player/fred.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>fred</title>
+<meta charset="UTF-8">
+<!--
+<div>Icons made by <a href="https://www.flaticon.com/authors/daniele-de-santis" title="Daniele De Santis">Daniele De Santis</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+-->
+<!--tutorial for seek slider: https://www.developphp.com/video/JavaScript/Audio-Seek-and-Volume-Range-Slider-Tutorial -->
+<style>
+	body {
+		width: 100%;
+	}
+
+	/*inputs */
+	.input {
+		outline: none;
+	}
+
+	.input #seekslider{
+		width: 100%;
+	}
+
+	.input #volslider{
+	width: 80px;
+	}
+
+	input[type='range'] {
+    	-webkit-appearance: none !important;
+		margin:0px;
+		padding:0px;
+    	background: #333;
+    	height:3px;
+		border-bottom:#333 1px solid;
+	}
+	
+	input[type='range']::-ms-fill-lower  {
+		background:#333;
+	}
+
+	input[type='range']::-ms-fill-upper  {
+		background:#333;
+	}
+	
+	input[type='range']::-moz-range-track {
+		border:none;
+    	background: #333;
+	}	
+
+	input[type='range']::-webkit-slider-thumb {
+    	-webkit-appearance: none !important;
+    	background: radial-gradient(#FFF, #333);
+    	height:11px;
+    	width:11px;
+		border-radius:100%;
+		cursor:pointer;
+	}
+
+	input[type='range']::-moz-range-thumb {
+    	background: radial-gradient(#FFF, #333);
+    	height:11px;
+    	width:11px;
+		border-radius:100%;
+		cursor:pointer;
+	}
+
+	input[type='range']::-ms-thumb {
+    	-webkit-appearance: none !important;
+    	background: radial-gradient(#FFF, #333);
+    	height:11px;
+    	width:11px;
+		border-radius:100%;
+		cursor:pointer;
+	}
+
+	button{
+		border: none;
+		cursor:pointer;
+		max-width:100%;
+		max-height:100%;
+		outline:none;
+		
+	}
+
+	button#playpausebtn{
+		background:url(icons/play-button.png) no-repeat;
+		height: 40px;
+		width: 40px; 
+		background-size: cover;
+	}
+
+	button#mutebtn{
+		background:url(icons/speaker.png) no-repeat;
+		height: 30px;
+		width: 30px; 
+		background-size: cover;
+	}
+</style>
+<script>
+	var audio, playbtn, mutebtn, seekslider, volumeslider, seeking=false, seekto;
+	function startAudioPlayer() {
+		audio = new Audio();
+		audio.src = "audio/jazzy.mp3";
+		
+		// set object refrences
+		
+
+		
+		//functions
+		
+		
+		// event handling
+
+		//var el = document.getElementById('overlayBtn');
+			//if(el){
+  			//el.addEventListener('click', swapper, false);
+		//}
+
+		//mutebtn.addEventListener("click", mute);
+	}
+	//toggle play
+	function playPause() {
+		if(audio.paused) {
+			audio.play();
+			document.getElementById("playpausebtn").style.backgroundImage = "url ('icons/pause.png') no repeat"
+		} else {
+			audio.pause();
+			document.getElementById("playpausebtn").style.backgroundImage = "url ('icons/play-button.png') no repeat"
+		}
+	}
+	//toggle mute
+	function mute() {
+		if(audio.muted) {
+			audio.muted = false;
+		} else {
+			audio.muted = true;
+		}
+	}
+	//seek
+	function seek(event) {
+		if(seeking) {
+			seekslider.value = event.clientX - seekslider.offsetLeft;
+			seekto = audio.duration * (seekslider.value / 100);
+			audio.currentTime = seekto;
+		}
+	}
+	//volume
+	function setvolume() {
+		audio.volume = volslider.value / 100;
+	}
+
+	window.onload = startAudioPlayer();
+
+</script>
+
+
+</head>
+<body>
+	<div>
+		<button onclick="playPause()" id="playpausebtn"></button>
+		<div>
+			<input id="seekslider" onmouseup="function(){ seeking=false; }" onmousedown="function(event){ seeking=true; seek(event); }" onmousemove="function(event){ seek(event); }" type="range" min="0" max="100" value="0" step="1">
+
+			<!--
+			Seek slider ^^^^^ code error is somewhere in here
+
+
+			 onmouseup="function() {seeking=false; };" 
+			onmousedown=" function(event) { seeking=true; seek(event); };" 
+			onmousemove="function(event) { seek(event); };" -->
+		</div>
+	</div>
+	<div>
+		<button onclick="mute()" id="mutebtn"></button>
+		<div>
+			<input id="volslider" onmousemove="setvolume()" type="range" min="0" max="100" value="100" step="1">	
+		</div>	
+	</div>
+	<div class="todo">
+		
+		<b style="font-size: 16px">To Do:</b>
+		<br /><br />
+
+		<ol>
+			<li>Make song buttons work.</li>
+			<li>Make play / pause buttons work.</li>
+			<li>Make next button work.</li>
+			<li>Make prev button work.</li>
+			<li>When song changes, update song title.</li>
+			<li>When song changes, update song img.</li>
+			<li>As track plays, update song track time display.</li>
+			<li>As track plays, update song track marker.</li>
+
+		</ol>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/daniele-de-santis" title="Daniele De Santis">Daniele De Santis</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	</div>
+</body>
+</html>
