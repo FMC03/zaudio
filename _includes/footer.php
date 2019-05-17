9475402a2c707f39c059481046df65125297215a
Fred <boromirfmcjr91@gmail.com>
2019-05-17 14:10:55 -0400

    I made it look pretty

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,18 +1,7 @@
 <div class="Client-Currently-Playing">
 	<div class="Currently-Playing-Container">
 
-		<div style="background-image: url(client/images/sm.jpg);"  class="Song-IMG"></div>
-
-		<div class="Song-Information">
-			<b class="Song-Playing">CURRENTLY PLAYING</b>
-			<b class="Song-Name">Sicko Mode</b>
-			<b class="Song-Artist">Travis Scott</b>
-		</div>
-		<div class="Song-Options">
-			<button onclick="goBack()" id="arrow skip">PREV</button>
-			<button onclick="togglePlayState()" id="playpausebtn" >PLAY / PAUSE</button>
-			<button onclick="playNextSong()" id="arrow last">NEXT</button>
-		</div>
+		
 	</div>
 </div>
 
