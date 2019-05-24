f719bd05d05d0d7ec627d9870f1edae33dc4b118
Rob Korobkin <rob.korobkin@gmail.com>
2019-05-24 12:44:00 -0400

    Fixing HTML / PHP

---
+++ v2/client/list.js
@@ -0,0 +1,32 @@
+tracklist_data = [
+	// songs[0]
+	{
+		"song": "Church Oragon",
+		"artist" : "Baxter Media",
+		"img": "client/img/co.jpg",
+		"likes" : "0",
+		"replays" : "0",
+		"audio" : "client/songs/churchoragon.mp3"
+	},
+
+	
+	// songs[1]
+	{
+		"song" : "Emilyblenk",
+		"artist" : "Baxter Media",
+		"img" : "client/img/eb.jpg",
+		"likes" : "0",
+		"replays" : "0",
+		"audio" : "client/songs/emilyblenk.mp3"
+	},
+
+	// songs[2]
+	{
+		"song" : "Mobile Games",
+		"artist" : "Baxter Media",
+		"img" : "client/img/mgl.jpg",
+		"likes" : "0",
+		"replays" : "0",
+		"audio" : "client/songs/Mobile Games.mp3"
+	}
+]
